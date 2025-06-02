09153fe7fe3f4cbb7464b4a67bcb04aedde38333
Popa Marian-Iulian <popaiulian253@gmail.com>
2025-06-02 18:16:47 +0300

    creating movies carousel

--- movie-pal/src/app/pages/home/home.component.ts
+++ movie-pal/src/app/pages/home/home.component.ts
@@ -16,9 +16,12 @@ const BASE_API_URL = 'http://localhost:8080/api';
 export class HomeComponent implements OnInit {
   movies: featuredMovie[] = [];
   page = 0;
-  size = 5;
+  size = 10;
   loading = false;
   allLoaded = false;
+
+  currentPage = 0;
+  moviesPerPage = 5;
 
   constructor(private http: HttpClient) {}
 
@@ -35,31 +38,56 @@ export class HomeComponent implements OnInit {
   }
 
   loadMovies(): void {
-  this.loading = true;
-  console.log('Fetching movies with parameters:', { page: this.page, size: this.size });
-  
-  this.http.get<featuredMovie[]>(`${BASE_API_URL}/movies?page=${this.page}&size=${this.size}`)
-    .subscribe({
-      next: (data) => {
-        console.log('API Response For Movies:', data);
-        
-        if (data.length > 0) {
-          this.movies.push(...data);
-          this.page++;
-          console.log('Updated movies array:', this.movies);
-          console.log('New page number:', this.page);
-        } else {
-          this.allLoaded = true;
-          console.log('All movies loaded, no more data available');
+    this.loading = true;
+    console.log('Fetching movies with parameters:', { page: this.page, size: this.size });
+    
+    this.http.get<featuredMovie[]>(`${BASE_API_URL}/movies?page=${this.page}&size=${this.size}`)
+      .subscribe({
+        next: (data) => {
+          console.log('API Response For Movies:', data);
+          
+          if (data.length > 0) {
+            this.movies.push(...data);
+            this.page++;
+            console.log('Updated movies array:', this.movies);
+            console.log('New page number:', this.page);
+          } else {
+            this.allLoaded = true;
+            console.log('All movies loaded, no more data available');
+          }
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Error fetching movies:', error);
+          this.loading = false;
         }
-        this.loading = false;
-      },
-      error: (error) => {
-        console.error('Error fetching movies:', error);
-        this.loading = false;
-      }
-    });
-}
+      });
+  }
 
-  
+  get paginatedMovies(): featuredMovie[]{
+    const start = this.currentPage * this.moviesPerPage;
+    const end = start + this.moviesPerPage;
+
+    if (end + this.moviesPerPage > this.movies.length && !this.allLoaded && !this.loading) {
+      this.loadMovies();
+    }
+
+    return this.movies.slice(start, end);
+  }
+
+  nextPage(): void {
+    if (this.hasMoreMovies()){
+      this.currentPage++;
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
+
+  hasMoreMovies(): boolean {
+    return (this.currentPage + 1) * this.moviesPerPage < this.movies.length || !this.allLoaded;
+  }
 }
